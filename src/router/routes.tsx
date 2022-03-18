a2ef6a42f8756391617cd00776d9aa05c69176ae
Ligalaiz <slavakuz88@gmail.com>
2022-03-18 13:43:57 +0300

    feat: STARDB-4 add HomePage component

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,8 @@
 import { Error } from '@components/Error';
+import { HomePage } from '@components/HomePage';
 
 export const routes = [
+  { path: '/', element: HomePage },
   { path: 'error', element: Error },
   { path: '*', element: Error },
 ];
