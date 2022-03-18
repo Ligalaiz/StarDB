df089f790f4235a8c161bb31d1a8bc1da0ed77e9
Ligalaiz <slavakuz88@gmail.com>
2022-03-18 20:17:03 +0300

    refactor: STARDB-5 change test for PeoplePage component

--- src/components/PeoplePage/PeoplePage.test.tsx
+++ src/components/PeoplePage/PeoplePage.test.tsx
@@ -6,12 +6,12 @@ import { PeoplePage } from '@components/PeoplePage';
 const renderComponent = () => render(<PeoplePage />);
 
 describe('PeoplePage component', () => {
-  describe('Component not exist', () => {
-    it('peoplePage check that the component has not been rendered #smoke', () => {
+  describe('Component exist', () => {
+    it('peoplePage check that the component has been rendered #smoke', () => {
       const { queryByTestId } = renderComponent();
       const peoplePage = queryByTestId('peoplePage');
 
-      expect(peoplePage).not.toBeInTheDocument();
+      expect(peoplePage).toBeInTheDocument();
     });
   });
 });
