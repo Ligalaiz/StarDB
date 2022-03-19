3a970de77c7f949798a751f5fd67767e9568e021
Ligalaiz <slavakuz88@gmail.com>
2022-03-19 19:48:32 +0300

    refactor: STARDB-6 change test for PlanetsPage component

--- src/components/PlanetsPage/PlanetsPage.test.tsx
+++ src/components/PlanetsPage/PlanetsPage.test.tsx
@@ -6,12 +6,12 @@ import { PlanetsPage } from '@components/PlanetsPage';
 const renderComponent = () => render(<PlanetsPage />);
 
 describe('PlanetsPage component', () => {
-  describe('Component not exist', () => {
-    it('planetsPage check that the component has not been rendered #smoke', () => {
+  describe('Component exist', () => {
+    it('planetsPage check that the component has been rendered #smoke', () => {
       const { queryByTestId } = renderComponent();
       const planetsPage = queryByTestId('planetsPage');
 
-      expect(planetsPage).not.toBeInTheDocument();
+      expect(planetsPage).toBeInTheDocument();
     });
   });
 });
