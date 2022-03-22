c7410408130568598ab1a839eb6c02612982551c
Ligalaiz <slavakuz88@gmail.com>
2022-03-22 18:21:47 +0300

    refactor: STARDB-11 add work with store for PlanetsPage

---
+++ src/modules/PlanetsPage/PlanetsPage.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { ExportPlanetPage } from './ExportPlanetPage';
+
+const PlanetsPage: FC = (props: any) => <ExportPlanetPage {...props} />;
+
+export { PlanetsPage };
