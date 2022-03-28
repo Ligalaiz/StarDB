3236f9eada7a126e256a91bf2da1d95c46e45481
Ligalaiz <slavakuz88@gmail.com>
2022-03-28 18:52:13 +0300

    refactor: STARDB-12 add routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,12 +5,17 @@ import { PlanetsPage } from '@modules/PlanetsPage';
 import { StarshipsPage } from '@modules/StarshipsPage';
 import { LoginPage } from '@components/LoginPage';
 import { SecretPage } from '@components/SecretPage';
+import { DetailsPage } from '@components/DetailsPage';
 
 export const routes = [
   { path: '/', element: HomePage },
+  { path: 'details/:id', element: DetailsPage },
   { path: 'people', element: PeoplePage },
+  { path: 'people/:id', element: PeoplePage },
   { path: 'planets', element: PlanetsPage },
+  { path: 'planets/:id', element: PlanetsPage },
   { path: 'starships', element: StarshipsPage },
+  { path: 'starships/:id', element: StarshipsPage },
   { path: 'login', element: LoginPage },
   { path: 'secret', element: SecretPage },
   { path: 'error', element: Error },
