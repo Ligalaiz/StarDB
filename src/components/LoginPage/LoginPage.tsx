544c1508980454de6e78e84f3d177d3e44404e67
Ligalaiz <slavakuz88@gmail.com>
2022-03-31 13:27:30 +0300

    refactor: STARDB-13 add redirect after login event

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
 import { useAction } from '@src/hooks/useAction';
+import { useNavigate } from 'react-router-dom';
 import * as l from './LoginPage.style';
 
 const LoginPage: FC = () => {
   const { isLogin } = useTypedUseSelector((state) => state.auth);
   const { login, logout } = useAction();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     if (isLogin) {
@@ -13,11 +15,14 @@ const LoginPage: FC = () => {
     } else {
       login();
     }
+    navigate('/');
   };
 
   return (
     <div data-testid="loginPage">
-      <p css={l.title}>Please login before u can use our site</p>
+      {!isLogin ? (
+        <p css={l.title}>Please login before u can use our site</p>
+      ) : null}
       <button onClick={handleClick} css={l.btn}>
         {isLogin ? 'Logout' : 'Login'}
       </button>
