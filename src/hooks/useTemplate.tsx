5d61b042e89193ab4a957a604db0691e6768cba5
Ligalaiz <slavakuz88@gmail.com>
2022-03-31 13:25:12 +0300

    refactor: STARDB-13 decomposition Temlate

--- src/hooks/useTemplate.tsx
+++ src/hooks/useTemplate.tsx
@@ -1,12 +1,11 @@
 import React, { FC, useEffect } from 'react';
 import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
 import { useAction } from '@src/hooks/useAction';
-import { errorBoundary } from '@src/HOC/errorBoundary';
-import { useParams, useNavigate } from 'react-router-dom';
-import { List } from '@components/List';
-import { Card } from '@components/Card';
-import * as t from './useTemplate.style';
+import { useParams } from 'react-router-dom';
+import { View } from '@components/Views/Views';
 import { getLocalDataUtils } from '@src/utils';
+import { fieldsMap } from '@components/Views/fieldsMap';
+import * as t from './useTemplate.style';
 
 interface ITemplate {
   path: string;
@@ -14,31 +13,15 @@ interface ITemplate {
   isDetails?: boolean;
 }
 
-interface IfieldsMap {
-  people: string[];
-  starships: string[];
-  planets: string[];
-  [key: string]: string[];
-}
-
-const fieldsMap: IfieldsMap = {
-  people: ['name', 'gender', 'eye_color'],
-  starships: ['model', 'craft_class', 'lenght', 'cost'],
-  planets: ['name', 'pupulation', 'rotation_period'],
-};
-
-const ListWithErrorBoundary = errorBoundary(List);
-const CardWithErrorBoundary = errorBoundary(Card);
-
 const Template: FC<ITemplate> = ({
   path,
   isHome = false,
   isDetails = false,
 }) => {
-  const { data, currentData, currentID, loadingData, sourceData } =
-    useTypedUseSelector((state) => state.data);
+  const { data, currentData, currentID, sourceData } = useTypedUseSelector(
+    (state) => state.data,
+  );
   const { dataRequest, setCurrentData, setCurrentID, setSource } = useAction();
-  const navigate = useNavigate();
   const { id } = useParams();
   let currentTarget: { [key: string]: string } | null = null;
 
@@ -72,63 +55,6 @@ const Template: FC<ITemplate> = ({
     }
   }, [data, id, path, setCurrentData, setCurrentID]);
 
-  let currentArrayData = data;
-
-  if (data) {
-    currentArrayData = [{ type: path }, ...data];
-  }
-
-  const regularView = (
-    <>
-      <div css={t.itemWrap}>
-        <ListWithErrorBoundary
-          data={currentArrayData}
-          isLoading={loadingData}
-        />
-      </div>
-
-      <div css={t.itemWrap}>
-        <CardWithErrorBoundary data={currentTarget} isLoading={loadingData} />
-      </div>
-    </>
-  );
-
-  const detailsView = (
-    <>
-      <div css={t.itemWrapDetails}>
-        <CardWithErrorBoundary data={currentTarget} isLoading={loadingData} />
-      </div>
-      <div css={t.itemWrapDetails}>
-        <button
-          css={t.btn}
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Back
-        </button>
-      </div>
-    </>
-  );
-
-  const homeView = (
-    <div css={t.itemWrapHome}>
-      <ListWithErrorBoundary
-        data={currentArrayData}
-        isLoading={loadingData}
-        isHome
-      />
-    </div>
-  );
-
-  let currentView = regularView;
-
-  if (isDetails) {
-    currentView = detailsView;
-  } else if (isHome) {
-    currentView = homeView;
-  }
-
   let testAttribute = path;
 
   if (isHome) {
@@ -139,7 +65,12 @@ const Template: FC<ITemplate> = ({
 
   return (
     <div css={t.peopleWrap} data-testid={`${testAttribute}Page`}>
-      {currentView}
+      <View
+        path={path}
+        isDetails={isDetails}
+        currentTarget={currentTarget}
+        isHome={isHome}
+      />
     </div>
   );
 };
