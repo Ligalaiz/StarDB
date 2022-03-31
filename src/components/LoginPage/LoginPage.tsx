4af0c747e9d5dc062176e8ec5941bfea09a32749
Ligalaiz <slavakuz88@gmail.com>
2022-03-31 10:51:24 +0300

    refactor: STARDB-13 add login button

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from 'react';
+import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
+import { useAction } from '@src/hooks/useAction';
 import * as l from './LoginPage.style';
 
 const LoginPage: FC = () => {
+  const { isLogin } = useTypedUseSelector((state) => state.auth);
+  const { login, logout } = useAction();
+
+  const handleClick = () => {
+    if (isLogin) {
+      logout();
+    } else {
+      login();
+    }
+  };
+
   return (
     <div data-testid="loginPage">
-      <p css={l.title}>Login to see secret page!</p>
-      <button css={l.btn}>Login</button>
+      <p css={l.title}>Please login before u can use our site</p>
+      <button onClick={handleClick} css={l.btn}>
+        {isLogin ? 'Logout' : 'Login'}
+      </button>
     </div>
   );
 };
