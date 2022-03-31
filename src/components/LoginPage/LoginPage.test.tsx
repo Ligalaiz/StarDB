544f3270ebf68555e4dce6f704492fd817cd21fe
Ligalaiz <slavakuz88@gmail.com>
2022-03-31 13:52:15 +0300

    test: STARDB-13 add router for LoginPage test

--- src/components/LoginPage/LoginPage.test.tsx
+++ src/components/LoginPage/LoginPage.test.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { LoginPage } from '@components/LoginPage';
 import { Provider } from 'react-redux';
 import { store } from '@src/store';
 
 const renderComponent = () =>
   render(
-    <Provider store={store}>
-      <LoginPage />
-    </Provider>,
+    <Router>
+      <Provider store={store}>
+        <LoginPage />
+      </Provider>
+    </Router>,
   );
 
 describe('LoginPage component', () => {
